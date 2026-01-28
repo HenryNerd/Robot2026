d3ab39f4dd2f958c3630448c185f852f0023aa2d
Marcus Chumanov <marcus.chumanov@gmail.com>
2026-01-27 20:18:07 -0600

    added indexer commands

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.indexer;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.AutoLogOutput;
@@ -28,5 +31,23 @@ public class Indexer extends SubsystemBase {
   @AutoLogOutput
   public Command runSpeed(DoubleSupplier speed) {
     return runEnd(() -> indexerIO.setPower(speed.getAsDouble()), () -> indexerIO.setPower(0.0));
+  }
+
+  @AutoLogOutput
+  public Command instantRunPowerCommand(Indexer indexer, double power){
+    return (Commands.runOnce(() -> {
+      indexer.setPower(power);
+    }));
+  }
+
+  @AutoLogOutput
+  public Command startEndRunPowerCommand(Indexer indexer, double power){
+    return (Commands.startEnd(
+    () -> {
+      indexer.setPower(power);
+    },
+    () -> {
+      indexer.setPower(0);
+    }));
   }
 }
