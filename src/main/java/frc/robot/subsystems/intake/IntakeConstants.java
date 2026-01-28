a1a433b1778cb7d1a4a0a3cee7a2f834bcd5b59f
Eli Bailey <eli.bailey.376@gmail.com>
2026-01-27 19:11:32 -0600

    Move latch code to intake subsystem

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems.intake;
 
+import static edu.wpi.first.units.Units.Degrees;
+
+import edu.wpi.first.units.measure.Angle;
+
 public class IntakeConstants {
   public static int intakeLeftMotorId = 0;
   public static int intakeRightMotorId = 0;
+  public static int latchMotorId = 0;
+  public static Angle latchExtensionAngle = Degrees.of(0);
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOReal.java
@@ -12,6 +12,9 @@ public class IntakeIOReal implements IntakeIO {
   private final TalonFX rightMotor;
   private final DutyCycleOut dutyCycle;
 
+  private final TalonFX latchMotor;
+  private final StatusSignal<Current> latchCurrentSignal;
+
   private final StatusSignal<Current> leftCurrentSignal;
   private final StatusSignal<Current> rightCurrentSignal;
 
@@ -22,6 +25,9 @@ public class IntakeIOReal implements IntakeIO {
     leftCurrentSignal = leftMotor.getSupplyCurrent();
     rightCurrentSignal = rightMotor.getSupplyCurrent();
 
+    latchMotor = new TalonFX(IntakeConstants.latchMotorId);
+    latchCurrentSignal = latchMotor.getSupplyCurrent();
+
     dutyCycle = new DutyCycleOut(0).withEnableFOC(true);
   }
 
@@ -29,18 +35,26 @@ public class IntakeIOReal implements IntakeIO {
   public void updateInputs(IntakeIOInputs inputs) {
     inputs.isLeftMotorConnected = leftMotor.isConnected();
     inputs.isRightMotorConnected = rightMotor.isConnected();
+    inputs.isLatchMotorConnected = latchMotor.isConnected();
     inputs.leftVelocity = leftMotor.getVelocity().getValue();
     inputs.rightVelocity = rightMotor.getVelocity().getValue();
+    inputs.latchPosition = latchMotor.getPosition().getValue();
     inputs.leftTemp = leftMotor.getDeviceTemp().getValue();
     inputs.rightTemp = rightMotor.getDeviceTemp().getValue();
+    inputs.latchTemp = latchMotor.getDeviceTemp().getValue();
 
-    BaseStatusSignal.refreshAll(leftCurrentSignal, rightCurrentSignal);
+    BaseStatusSignal.refreshAll(leftCurrentSignal, rightCurrentSignal, latchCurrentSignal);
     inputs.leftCurrent = leftCurrentSignal.getValue();
     inputs.rightCurrent = rightCurrentSignal.getValue();
+    inputs.latchCurrent = latchCurrentSignal.getValue();
   }
 
   @Override
   public void set(double power) {
     leftMotor.setControl(dutyCycle.withOutput(power));
+  }
+  @Override
+  public void extend() {
+    latchMotor.setPosition(IntakeConstants.latchExtensionAngle);
   }
 }
