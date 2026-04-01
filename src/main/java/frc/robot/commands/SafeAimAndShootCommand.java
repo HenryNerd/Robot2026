71319bb06fcb2218fbb5636e763fdec0557dcfd9
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2026-04-01 18:00:38 -0500

    copy from auto ranging

--- src/main/java/frc/robot/commands/SafeAimAndShootCommand.java
+++ src/main/java/frc/robot/commands/SafeAimAndShootCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.drive.Drive;
@@ -13,8 +14,12 @@ import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
+import static edu.wpi.first.units.Units.Feet;
+
 public class SafeAimAndShootCommand extends ParallelCommandGroup {
 
+  private static final Distance MINIMUM_SHOT_DISTANCE = Feet.of(7.5);
+  
   public SafeAimAndShootCommand(
       Drive drive,
       Shooter shooter,
@@ -45,6 +50,21 @@ public class SafeAimAndShootCommand extends ParallelCommandGroup {
     Command driveAtAngleCommand =
         DriveCommands.driveAimLockedCommand(drive, xSupplier, ySupplier, positionSupplier, true);
 
-    addCommands(safeShootCommand, driveAtAngleCommand);
+    Command moveIntoRangeCommand =
+            new MoveIntoRangeCommand(drive, positionSupplier.get(), MINIMUM_SHOT_DISTANCE);
+
+    BooleanSupplier fartherThanCondition =
+            () -> drive.fartherThan(positionSupplier.get(), MINIMUM_SHOT_DISTANCE);
+
+    BooleanSupplier driveAngleCondition = () -> drive.isLocked(drive, positionSupplier.get(), true, angleTolerance);
+
+    Command guardedRangeCommand =
+            new GuardedCommand(
+                    moveIntoRangeCommand,
+                    driveAngleCondition,
+                    () -> !fartherThanCondition.getAsBoolean());
+//                    () -> !autoRangingOverride.getAsBoolean());
+    
+    addCommands(safeShootCommand, driveAtAngleCommand, guardedRangeCommand);
   }
 }
